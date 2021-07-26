fa07911f3dbe013068c6472a740763ddaf60f67f
Luigi Greco <9luigi1@gmail.com>
2021-07-26 23:57:07 +0200

    Added signature and metadata for: Big Little Lies S02E01, Chernobyl S01E01, The Undoing S01E01, Twin Peaks S01E01

--- TVid/EpisodeData.swift
+++ TVid/EpisodeData.swift
@@ -47,5 +47,29 @@ let episodes = [
         episodeNumber: "Episodio del 11/05/2021",
         additionalInfo: "Frank Matano e il cantautore Ghemon ospiti della puntata. \"Una pezza di Lundini\" è anche un programma di informazione catapultato in una dimensione disturbante e surreale. Il conduttore \"inadeguato\" è Valerio Lundini, in studio con lui Emanuela Fanelli e la band dei Vazzanikki.",
         thumbnailURLPath: "https://artworks.thetvdb.com/banners/series/388084/posters/5fe512c64ef02.jpg"
-    )
+    ),
+    EpisodeData(
+        title: "Big Little Lies",
+        signatureFilename: "biglittlelies-s02e01",
+        episodeNumber: "S02E01 What Have They Done?",
+        additionalInfo: "Following first-day-of-school events, Madeline is worried by Bonnie’s behavior. Mary Louise, Celeste’s mother-in-law, offers her unvarnished assessment of Madeline’s character. Jane learns that she’s known in town as one of the “Monterey Five.”",
+        thumbnailURLPath: "https://artworks.thetvdb.com/banners/seasons/5cffe05d340d7.jpg"),
+    EpisodeData(
+        title: "Chernobyl",
+        signatureFilename: "chernobyl-s01e01",
+        episodeNumber: "S01E01 1:23:45",
+        additionalInfo: "Plant workers and firefighters put their lives on the line to control a catastrophic 1986 explosion at a Soviet nuclear power plant.",
+        thumbnailURLPath: "https://artworks.thetvdb.com/banners/posters/5cc12861c93e4.jpg"),
+    EpisodeData(
+        title: "The Undoing",
+        signatureFilename: "theundoing-s01e01",
+        episodeNumber: "S01E01 The Undoing",
+        additionalInfo: "Grace Fraser becomes intrigued by a young mother at her son's school. News of a tragedy rocks the school community.",
+        thumbnailURLPath: "https://artworks.thetvdb.com/banners/series/369307/posters/5fa360bfbe810.jpg"),
+    EpisodeData(
+        title: "Twin Peaks",
+        signatureFilename: "twinpeaks-s01e01",
+        episodeNumber: "S01E01 Northwest Passage",
+        additionalInfo: "The small northwest town of Twin Peaks, Washington is shaken up when the body of the Homecoming Queen, Laura Palmer, is discovered washed up on a riverbank, wrapped in plastic. Meanwhile, the rebellious Audrey Horne ruins a business deal for her father Benjamin.",
+        thumbnailURLPath: "https://artworks.thetvdb.com/banners/seasons/70533-1-3.jpg")
 ]
